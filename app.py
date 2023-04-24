c0812991be135a4f50e7de86350c811b0c03261d
Amir Meimand <amirmeimand@gmail.com>
2023-04-24 14:47:04 -0700

    Update app.py
    Add save functionality

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ import plotly.express as px
 from sklearn.cluster import KMeans, BisectingKMeans, MiniBatchKMeans
 from sklearn.mixture import GaussianMixture
 
+from io import StringIO
 
 import streamlit as st
 
@@ -93,13 +94,13 @@ def show_palette(palette_hex):
     return fig
 
 
-def store_palette(palette):
+def store_palette(palette,tab):
     """store palette colors in session state"""
     palette_size = len(palette)
     columns = st.columns(palette_size)
     for i, col in enumerate(columns):
         with col:        
-            st.session_state[f"col_{i}"]= st.color_picker(label=str(i), value=rgb_to_hex(palette[i]), key=f"pal_{i}")
+            st.session_state[f"col_{i}"]= st.color_picker(label=str(i), value=rgb_to_hex(palette[i]), key=f"pal_{i}_{tab}")
 
 def display_matplotlib_code(palette_hex):
 
@@ -171,27 +172,7 @@ st.sidebar.markdown("---")
 st.sidebar.header("Settings")
 palette_size = int(st.sidebar.number_input("palette size", min_value=1, max_value=20, value=5, step=1, help="Number of colors to infer from the image."))
 sample_size = 1000#int(st.sidebar.number_input("sample size", min_value=5, max_value=3000, value=500, step=500, help="Number of sample pixels to pick from the image."))
-
-# Image Enhancement
 enhancement_categories = enhancement_range.keys()
-#enh_expander = st.sidebar.expander("Image Enhancements", expanded=False)
-#with enh_expander:
-    
-#    if st.button("reset"):
-#        for cat in enhancement_categories:
-#            if f"{cat}_enhancement" in st.session_state:
-#                st.session_state[f"{cat}_enhancement"] = 1.0
-#enhancement_factor_dict = {
-#    cat: enh_expander.slider(f"{cat} Enhancement", 
-#                            value=1., 
-#                            min_value=enhancement_range[cat][0], 
-#                            max_value=enhancement_range[cat][1], 
-#                            step=enhancement_range[cat][2],
-#                            key=f"{cat}_enhancement")
-#    for cat in enhancement_categories
-#}
-#enh_expander.info("**Try the following**\n\nColor Enhancements = 2.6\n\nContrast Enhancements = 1.1\n\nBrightness Enhancements = 1.1")
-
 # Clustering Model 
 model_name = 'KMeans' #st.sidebar.selectbox("machine learning model", model_dict.keys(), help="Machine Learning model to use for clustering pixels and colors together.")
 sklearn_info = st.sidebar.empty()
@@ -202,15 +183,166 @@ sort_func = 'sqr_rgb'#st.sidebar.selectbox("palette sort function", options=sort
 # Random Number Seed
 seed = 42#int(st.sidebar.number_input("random seed", value=42, help="Seed used for all random samplings."))
 np.random.seed(seed)
-#st.sidebar.markdown("---")
+
 
 
 # =======
 #   App
 # =======
 
+def save_result(palette_hex,tab):
+    palette_title = st.text_input('Enter a name to save your palette:','my_palette',key=tab)
+    palette_title = palette_title.upper()
+    save = st.button('Save palette',key=f"save palette_{tab}")
+    if save:
+        saved_palette = '|'.join(palette_hex)        
+        
+        try:
+            current = pd.read_csv('saved_palette.csv')  
+            current = current.set_index('title')['colors'].to_dict()
+                          
+            if saved_palette in current.values():
+                key_found = next((key for key, value in current.items() if value == saved_palette), None)
+
+                st.warning(f"This palette is already saved as {key_found}")
+            if palette_title in current.keys():
+                st.warning('This title is already exist. Please select a different title to save the palette. ')
+            if ((saved_palette not in current.values())  and (palette_title not in current.keys())):
+                saved_palette = pd.DataFrame([[palette_title,saved_palette]],columns=['title','colors'])                
+                saved_palette.to_csv('saved_palette.csv',mode='a',header=False)
+                st.success('Palette is saved successfully!', icon="✅")
+        except: 
+            saved_palette = pd.DataFrame([[palette_title,saved_palette]],columns=['title','colors'])
+            saved_palette.to_csv('saved_palette.csv')
+            st.success('Palette is saved successfully!', icon="✅")
+    
+# show the image
+def show_image(tab,click,toggle,img,sample_size):
+    
+# convert RGBA to RGB if necessary
+    n_dims = np.array(img).shape[-1]
+    if n_dims == 4:
+        background = Image.new("RGB", img.size, (255, 255, 255))
+        background.paste(img, mask=img.split()[3]) # 3 is the alpha channel
+        img = background
+    
+    with st.expander("", expanded=True):
+        st.image(img, use_column_width=True)
+    
+    if click or toggle:
+        
+        df_rgb = get_df_rgb(img, sample_size)
+    
+        
+        # calculate the RGB palette and cache it to session_state
+        st.session_state["palette_rgb"] = get_palette(df_rgb, model_name, palette_size, sort_func=sort_func)
+    
+        if "palette_rgb" in st.session_state:
+            
+            # store individual colors in session state
+            store_palette(st.session_state["palette_rgb"],tab)
+    
+            st.write("---")
+    
+            # sort the colors based on the selected option
+            colors = {k: v for k, v in st.session_state.items() if k.startswith("col_")}
+            sorted_colors = {k: colors[k] for k in sorted(colors, key=lambda k: int(k.split("_")[-1]))}
+            
+            # find the hex representation for matplotlib and plotly settings
+            palette_hex = [color for color in sorted_colors.values()][:palette_size]
+            
+            # saved
+            #save_result(palette_hex,tab)
+            #saved         
+            with st.expander("Adopt this Palette", expanded=False):
+                st.pyplot(show_palette(palette_hex))
+    
+                matplotlib_tab, plotly_tab = st.tabs(["matplotlib", "plotly"])
+    
+                with matplotlib_tab:
+                    display_matplotlib_code(palette_hex)
+    
+                    import matplotlib as mpl
+                    from cycler import cycler
+    
+                    mpl.rcParams["axes.prop_cycle"] = cycler(color=palette_hex)
+                    import matplotlib.pyplot as plt
+    
+                    x = np.arange(5)
+                    y_list = np.random.random((len(palette_hex), 5))+2
+                    df = pd.DataFrame(y_list).T
+    
+                    area_tab, bar_tab = st.tabs(["area chart", "bar chart"])
+    
+                    with area_tab:
+                        fig_area , ax_area = plt.subplots()
+                        df.plot(kind="area", ax=ax_area, backend="matplotlib", )  
+                        st.header("Example Area Chart")
+                        st.pyplot(fig_area)
+        
+                    with bar_tab:
+                        fig_bar , ax_bar = plt.subplots()
+                        df.plot(kind="bar", ax=ax_bar, stacked=True, backend="matplotlib", )
+                        st.header("Example Bar Chart")
+                        st.pyplot(fig_bar)
+    
+                    
+                with plotly_tab:
+                    display_plotly_code(palette_hex)
+    
+                    import plotly.io as pio
+                    import plotly.graph_objects as go
+                    pio.templates["sophisticated"] = go.layout.Template(
+                        layout=go.Layout(
+                        colorway=palette_hex
+                        )
+                    )
+                    pio.templates.default = 'sophisticated'
+    
+                    area_tab, bar_tab = st.tabs(["area chart", "bar chart"])
+    
+                    with area_tab:
+                        fig_area = df.plot(kind="area", backend="plotly", )
+                        st.header("Example Area Chart")
+                        st.plotly_chart(fig_area, use_container_width=True)
+        
+                    with bar_tab:
+                        fig_bar = df.plot(kind="bar", backend="plotly", barmode="stack")
+                        st.header("Example Bar Chart")
+                        st.plotly_chart(fig_bar, use_container_width=True)
+    
+            with st.expander("Save this Palette", expanded=False):
+                    save_result(palette_hex,tab)
+    else:
+        st.info("Click on 'Find Palette' ot turn on 'Toggle Update' to see the color palette.")
+
+
+               
 # provide options to either select an image form the gallery, upload one, or fetch from URL
-gallery_tab, upload_tab  = st.tabs(["Gallery", "Upload"])
+gallery_tab, upload_tab,saved_tab,admin  = st.tabs(["Gallery", "Upload", "Saved","Admin"])
+
+
+with saved_tab:
+    
+    try:
+        current = pd.read_csv('saved_palette.csv')
+        current_data = current[['title','colors']].to_csv()    
+        st.download_button("Dowload all saved palette as a csv file",data=current_data, file_name='saved_palette.csv',mime='text/csv')
+        
+        current = current.set_index('title')['colors'].to_dict()
+        select_key = st.multiselect('Select saved palette(s) to see the color codes: ', current.keys(),help='If you do not see your saved palette please refresh your browser!')
+        selected_saved = {k: v for k, v in current.items() if k in select_key}
+    
+        for key, value in selected_saved.items():
+            st.subheader(key)
+            st.pyplot(show_palette(value.split('|')),dpi=80)        
+            st.write(value.split('|')) 
+        
+    except:
+        st.warning('Unable to load any saved palettes!')
+    
+   
+
 with gallery_tab:
     options = list(gallery_dict.keys())
     file_name = st.selectbox("Select a Logo", 
@@ -223,7 +355,8 @@ with gallery_tab:
         st.warning("To use the Gallery, remove the image URL first.")
 
     img = Image.open(file)
-
+    show_image('gallery',click,toggle,img,sample_size)
+    
 with upload_tab:
     file = st.file_uploader("Upload your own image", key="file_uploader")
     if file is not None:
@@ -233,108 +366,27 @@ with upload_tab:
             st.error("The file you uploaded does not seem to be a valid image. Try uploading a png or jpg file.")
     if st.session_state.get("image_url") not in ["", None]:
         st.warning("To use the file uploader, remove the image URL first.")
-
-
-# convert RGBA to RGB if necessary
-n_dims = np.array(img).shape[-1]
-if n_dims == 4:
-    background = Image.new("RGB", img.size, (255, 255, 255))
-    background.paste(img, mask=img.split()[3]) # 3 is the alpha channel
-    img = background
-
-# apply image enhancements
-#for cat in enhancement_categories:
-#    img = getattr(ImageEnhance, cat)(img)
-#    img = img.enhance(enhancement_factor_dict[cat])
-
-# show the image
-with st.expander("", expanded=True):
-    st.image(img, use_column_width=True)
-
-
-if click or toggle:
+    show_image('upload',click,toggle,img,sample_size)
     
-    df_rgb = get_df_rgb(img, sample_size)
-
-    # (optional for later)
-    # plot_rgb_3d(df_rgb) 
-    # plot_hsv_3d(df_rgb) 
-
-    # calculate the RGB palette and cache it to session_state
-    st.session_state["palette_rgb"] = get_palette(df_rgb, model_name, palette_size, sort_func=sort_func)
-
-    if "palette_rgb" in st.session_state:
-        
-        # store individual colors in session state
-        store_palette(st.session_state["palette_rgb"])
-
-        st.write("---")
-
-        # sort the colors based on the selected option
-        colors = {k: v for k, v in st.session_state.items() if k.startswith("col_")}
-        sorted_colors = {k: colors[k] for k in sorted(colors, key=lambda k: int(k.split("_")[-1]))}
-        
-        # find the hex representation for matplotlib and plotly settings
-        palette_hex = [color for color in sorted_colors.values()][:palette_size]
-        with st.expander("Adopt this Palette", expanded=False):
-            st.pyplot(show_palette(palette_hex))
-
-            matplotlib_tab, plotly_tab = st.tabs(["matplotlib", "plotly"])
-
-            with matplotlib_tab:
-                display_matplotlib_code(palette_hex)
-
-                import matplotlib as mpl
-                from cycler import cycler
-
-                mpl.rcParams["axes.prop_cycle"] = cycler(color=palette_hex)
-                import matplotlib.pyplot as plt
-
-                x = np.arange(5)
-                y_list = np.random.random((len(palette_hex), 5))+2
-                df = pd.DataFrame(y_list).T
-
-                area_tab, bar_tab = st.tabs(["area chart", "bar chart"])
-
-                with area_tab:
-                    fig_area , ax_area = plt.subplots()
-                    df.plot(kind="area", ax=ax_area, backend="matplotlib", )  
-                    st.header("Example Area Chart")
-                    st.pyplot(fig_area)
-    
-                with bar_tab:
-                    fig_bar , ax_bar = plt.subplots()
-                    df.plot(kind="bar", ax=ax_bar, stacked=True, backend="matplotlib", )
-                    st.header("Example Bar Chart")
-                    st.pyplot(fig_bar)
-
-                
-            with plotly_tab:
-                display_plotly_code(palette_hex)
-
-                import plotly.io as pio
-                import plotly.graph_objects as go
-                pio.templates["sophisticated"] = go.layout.Template(
-                    layout=go.Layout(
-                    colorway=palette_hex
-                    )
-                )
-                pio.templates.default = 'sophisticated'
-
-                area_tab, bar_tab = st.tabs(["area chart", "bar chart"])
-
-                with area_tab:
-                    fig_area = df.plot(kind="area", backend="plotly", )
-                    st.header("Example Area Chart")
-                    st.plotly_chart(fig_area, use_container_width=True)
-    
-                with bar_tab:
-                    fig_bar = df.plot(kind="bar", backend="plotly", barmode="stack")
-                    st.header("Example Bar Chart")
-                    st.plotly_chart(fig_bar, use_container_width=True)
-
-       
-else:
-    st.info("Click on 'Find Palette' ot turn on 'Toggle Update' to see the color palette.")
-
-   
+with admin:
+    pass_ = st.text_input('password',type='password',key='pass')
+    if pass_=='SnowFlake!':
+        try:
+            current = pd.read_csv('saved_palette.csv') 
+            st.table(current)
+            current_data = current.to_csv()    
+            st.download_button("Dowload all saved palette as a csv file",data=current_data, file_name='saved_palette.csv',mime='text/csv',key='dmin_dl')
+            trucate = st.button('trucate')
+            if trucate:
+                f = open('saved_palette.csv', "w+")
+                f.close()
+                saved_palette = pd.DataFrame([['','']],columns=['title','colors'])                
+                saved_palette.to_csv('saved_palette.csv',mode='a',header=False)
+        except:
+              st.write('nothing saved yet!')
+        uploaded_file = st.file_uploader("Choose a CSV file",type='csv' ,accept_multiple_files=False)
+        if uploaded_file is not None:
+            df=pd.read_csv(uploaded_file, error_bad_lines=True, warn_bad_lines=False)
+            replace = st.button('replace')
+            if replace:    
+                df.to_csv('saved_palette.csv')
